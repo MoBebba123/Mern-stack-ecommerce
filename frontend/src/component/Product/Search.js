1192dea2651658d9cf7943ab985337cead14f584
Mohamed Bebba <91488872+MoBebba123@users.noreply.github.com>
2022-02-01 23:24:16 +0100

    Update Search.js

--- frontend/src/component/Product/Search.js
+++ frontend/src/component/Product/Search.js
@@ -4,6 +4,20 @@ import "./Search.css";
 
 const Search = ({ history }) => {
   const [keyword, setKeyword] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const alert = useAlert();
+
+  const dispatch = useDispatch();
+
+  const { loading, error, products } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors);
+    }
+    dispatch(getProduct());
+  }, [dispatch, error, alert]);
 
   const searchSubmitHandler = (e) => {
     e.preventDefault();
@@ -14,18 +28,43 @@ const Search = ({ history }) => {
     }
   };
 
+  const ChangeHandler = (keyword) => {
+    let matches = []
+    if(text.length > 0){
+      matches = products.filter(product =>{
+        const regex = new RegExp(`${text}`, "gi");
+        return product.name.match(regex);
+      })
+    }
+    setSuggestions(matches)
+    setKeyword(keyword);
+
+  }
+
   return (
     <Fragment>
-      <MetaData title="Search A Product -- ECOMMERCE" />
-      <form className="searchBox" onSubmit={searchSubmitHandler}>
-        <input
-          type="text"
-          placeholder="Search a Product ..."
-          onChange={(e) => setKeyword(e.target.value)}
-        />
-        <input type="submit" value="Search" />
-      </form>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title="Search A Product -- ECOMMERCE" />
+          <form className="searchBox" onSubmit={searchSubmitHandler}>
+            <input
+              type="text"
+              placeholder="Search a Product ..."
+              value={keyword}
+              onChange={(e)=> ChangeHandler(e.target.value)}
+            />
+            <input type="submit" value="Search" />
+            {suggestions && suggestions.map((suggestion,i)=>(
+              <div key={i} className="suggestion"> {suggestion.name} </div>
+            ))}
+          </form>
+        </Fragment>
+      )
+      }
     </Fragment>
+
   );
 };
 
