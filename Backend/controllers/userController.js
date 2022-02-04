1614a5efb2df06b625d7e7fdf9dd0cb71aee81b5
Mohamed Bebba <91488872+MoBebba123@users.noreply.github.com>
2022-02-04 12:04:00 +0100

    Update userController.js

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -280,3 +280,30 @@ exports.deleteUser = catchAysncError(async (req, res, next) => {
     message: "User Deleted Successfully",
   });
 });
+
+// get users stats per month
+exports.getUsersStats = catchAysncError(async (req, res, next) => {
+
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
